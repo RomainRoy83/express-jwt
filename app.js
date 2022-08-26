f407e022f45304654c0a6834c1b37001f47e74ba
RomainRoy83 <romain.roy.83@gmail.com>
2022-08-26 16:14:58 +0200

    exercise done

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const { setupRoutes } = require('./routes');
-const express = require('express');
+const { setupRoutes } = require("./routes");
+const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
